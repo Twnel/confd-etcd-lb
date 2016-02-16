3c411e05004c883855d36990562f8c8c4220caa0
CoreOS Admin <ericson.cepeda@gmail.com>
2016-02-16 12:53:40 +0000

    [fix] HTTPS => HTTP consistency with ELB

--- nginx-skydns/update.sh
+++ nginx-skydns/update.sh
@@ -2,7 +2,7 @@ docker build -t twnel/nginx-sky:dev .
 
 docker rm -f nginx
 source /etc/environment;
-docker run --rm -ti --name nginx -p 81:81 -p 443:443 -p 80:80 --net host \
+docker run -d  --name nginx -p 81:81 -p 443:443 -p 80:80 --net host \
 -v /home/core/apn_cert/certs/:/etc/nginx/certs/ --privileged=true -e CLUSTER=beta \
 -e DOMAIN=twnel.me -e REGION=api -e HTPASSWD=$(etcdctl get /nginx/pwd) twnel/nginx-sky:dev
 docker logs nginx
