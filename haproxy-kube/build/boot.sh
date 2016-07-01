5bf56fa0c2d7ea0ff4bbede0f87a6aa9ca26c57e
ERICSON CEPEDA <ericson.cepeda@gmail.com>
2016-07-01 13:22:31 -0500

    [update] ENV consistence

--- haproxy-kube/build/boot.sh
+++ haproxy-kube/build/boot.sh
@@ -3,9 +3,9 @@
 # Fail hard and fast
 set -eo pipefail
 
-export ETCD_PORT=${ETCD_PORT:-2379}
-export HOST_IP=${HOST_IP:-172.17.42.1}
-export ETCD=$HOST_IP:$ETCD_PORT
+export ETCD_SERVICE_PORT=${ETCD_SERVICE_PORT:-2379}
+export ETCD_SERVICE_HOST=${ETCD_SERVICE_HOST:-172.17.42.1}
+export ETCD_SERVICE_ADDR=$ETCD_SERVICE_HOST:$ETCD_SERVICE_PORT
 export DOMAIN=${DOMAIN:-example.com}
 export REGION=${REGION:-api}
 export CLUSTER=${CLUSTER:-beta}
@@ -39,13 +39,13 @@ openssl req -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -x509 -nodes -days 365 -new
 echo "[haproxy] booting container. ETCD: $ETCD"
 
 # Loop until confd has updated the haproxy config
-until confd -onetime -node $ETCD; do
+until confd -onetime -node $ETCD_SERVICE_ADDR; do
   echo "[haproxy] waiting for confd to refresh haproxy.conf"
   sleep 5
 done
 
 # Run confd in the background to watch the upstream servers
-confd -interval 10 -node $ETCD &
+confd -interval 10 -node $ETCD_SERVICE_ADDR &
 echo "[haproxy] confd is listening for changes on etcd..."
 
 # Start haproxy
