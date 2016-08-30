d10903c46eebfb10f6c283326fb0df56f2160c66
ERICSON CEPEDA <ericson.cepeda@gmail.com>
2016-08-30 14:16:42 -0500

    ETCD resolve loop. LB internal with PodIP.

--- haproxy-kube/build/dns_check.py
+++ haproxy-kube/build/dns_check.py
@@ -11,6 +11,7 @@ import json
 from pprint import pformat, pprint
 # Basic query
 def populate_etcd():
+    kubectl_call_pods = "kubectl --namespace {} get -o json pods --selector=app={}"
     client = etcd.Client(host=os.getenv('ETCD_SERVICE_HOST', 'etcd'), port=int(os.getenv('ETCD_SERVICE_PORT', 2379)))
     while True:
         # modify DNS check to kube check
@@ -18,21 +19,30 @@ def populate_etcd():
         kubectl_call = subprocess.Popen("kubectl get --all-namespaces -o json services", stdout=subprocess.PIPE, shell=True)
         kubectl_str = str(kubectl_call.stdout.read())
         kubectl_services = json.loads(kubectl_str)
+        #for key in (app['key'] for app in client.get('/skydns/local/beta/ui')._children):
+        #    key.split('/')[-1]
         for service in kubectl_services['items']:
             for match_port in (port for port in service['spec']['ports'] if 'name' in port and 'internal' not in port['name']):
-                pprint(client.set(
-                    '/skydns/local/{}/{}/{}/{}:{}'.format(
-                        service['metadata']['app_env'] if 'app_env' in service['metadata'] else 'beta',
-                        service['metadata']['namespace'],
-                        match_port['name'],
-                        service['spec']['clusterIP'],
-                        match_port['port']
-                    ),
-                    json.dumps({
-                        'host': '{}.{}'.format(service['metadata']['name'], service['metadata']['namespace']),
-                        'port': match_port['port']
-                    })
-                ))
+                try:
+                    kubectl_get_pods = subprocess.Popen(kubectl_call_pods.format(service['metadata']['namespace'], service['spec']['selector']['app']), stdout=subprocess.PIPE, shell=True)
+                    kubectl_str_pods = str(kubectl_get_pods.stdout.read())
+                    kubectl_pods = json.loads(kubectl_str_pods)
+                    for pod in kubectl_pods['items']:
+                        pprint(client.set(
+                            '/skydns/local/{}/{}/{}/{}:{}'.format(
+                                service['metadata']['app_env'] if 'app_env' in service['metadata'] else 'beta',
+                                service['metadata']['namespace'],
+                                match_port['name'],
+                                pod['status']['podIP'],
+                                match_port['port']
+                            ),
+                            json.dumps({
+                                'host': pod['status']['podIP'],
+                                'port': match_port['port']
+                            })
+                        ))
+                except KeyError:
+                    pprint('Error: {}'.format(match_port['name']))
 
         time.sleep(os.getenv('ETCD_SERVICE_INTERVAL', 10))
 
--- nginx-skydns/boot.sh
+++ nginx-skydns/boot.sh
@@ -3,11 +3,14 @@
 # Fail hard and fast
 set -eo pipefail
 
-export ETCD_SERVICE_PORT=${ETCD_SERVICE_PORT:-2379}
-export ETCD_SERVICE_HOST=${ETCD_SERVICE_HOST:-127.0.0.1}
-export ETCD_RESOLVE=$(host ${ETCD_SERVICE_HOST} | awk '/has address/ { print $4 }')
-export ETCD_SERVICE_RESOLVE=${ETCD_RESOLVE:-${ETCD_SERVICE_HOST}}
-export ETCD_SERVICE_ADDR=${ETCD_SERVICE_RESOLVE}:${ETCD_SERVICE_PORT}
+confd_resolve () {
+  export ETCD_SERVICE_PORT=${ETCD_SERVICE_PORT:-2379}
+  export ETCD_SERVICE_HOST=${ETCD_SERVICE_HOST:-127.0.0.1}
+  export ETCD_RESOLVE=$(host ${ETCD_SERVICE_HOST} | awk '/has address/ { print $4 }')
+  export ETCD_SERVICE_RESOLVE=${ETCD_RESOLVE:-${ETCD_SERVICE_HOST}}
+  export ETCD_SERVICE_ADDR=${ETCD_SERVICE_RESOLVE}:${ETCD_SERVICE_PORT}
+}
+confd_resolve
 export DOMAIN=${DOMAIN:-example.com}
 export REGION=${REGION:-api}
 export CLUSTER=${CLUSTER:-beta}
@@ -38,16 +41,23 @@ emailAddress=
 echo "admin:${HTPASSWD}" > /etc/nginx/.htpasswd
 openssl req -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SSL_DIR/default.key" -out "$SSL_DIR/default.crt" -passin pass:$PASSPHRASE
 
-echo "[nginx] booting container. ETCD: $ETCD"
+echo "[nginx] booting container. ETCD: $ETCD_SERVICE_ADDR"
 
 # Loop until confd has updated the nginx config
-until confd -onetime -node $ETCD_SERVICE_ADDR; do
-  echo "[nginx] waiting for confd to refresh nginx.conf"
-  sleep 5
-done
+#until confd -onetime -node $ETCD_SERVICE_ADDR; do
+echo "[nginx] waiting for confd to refresh nginx.conf"
+#  sleep 5
+#done
 
 # Run confd in the background to watch the upstream servers
-confd -interval 10 -node $ETCD_SERVICE_ADDR &
+# confd -interval 10 -node $ETCD_SERVICE_ADDR &
+confd_interval () {
+  while true; do {
+    confd_resolve
+    confd -onetime -node $ETCD_SERVICE_ADDR; sleep 10;
+  }; done;
+}
+nohup confd_interval 2>&1 &
 echo "[nginx] confd is listening for changes on etcd..."
 
 # Start nginx
