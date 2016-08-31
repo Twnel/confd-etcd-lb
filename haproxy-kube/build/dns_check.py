0024742a38e5052841f9aa8d83b4f8da37ab73f2
ERICSON CEPEDA <ericson.cepeda@gmail.com>
2016-08-30 21:56:00 -0500

    [update] delete ETCD obsolete entries (generators + sets difference).

--- haproxy-kube/build/dns_check.py
+++ haproxy-kube/build/dns_check.py
@@ -10,6 +10,11 @@ import subprocess
 import json
 from pprint import pformat, pprint
 # Basic query
+def loop_services(kubectl_services):
+    for service in kubectl_services:
+        for match_port in (port for port in service['spec']['ports'] if 'name' in port and 'internal' not in port['name']):
+            yield service, match_port
+
 def populate_etcd():
     kubectl_call_pods = "kubectl --namespace {} get -o json pods --selector=app={}"
     client = etcd.Client(host=os.getenv('ETCD_SERVICE_HOST', 'etcd'), port=int(os.getenv('ETCD_SERVICE_PORT', 2379)))
@@ -19,30 +24,38 @@ def populate_etcd():
         kubectl_call = subprocess.Popen("kubectl get --all-namespaces -o json services", stdout=subprocess.PIPE, shell=True)
         kubectl_str = str(kubectl_call.stdout.read())
         kubectl_services = json.loads(kubectl_str)
-        #for key in (app['key'] for app in client.get('/skydns/local/beta/ui')._children):
-        #    key.split('/')[-1]
-        for service in kubectl_services['items']:
-            for match_port in (port for port in service['spec']['ports'] if 'name' in port and 'internal' not in port['name']):
+        def set_etcd_services():
+            for service, match_port in loop_services(kubectl_services['items']):
                 try:
                     kubectl_get_pods = subprocess.Popen(kubectl_call_pods.format(service['metadata']['namespace'], service['spec']['selector']['app']), stdout=subprocess.PIPE, shell=True)
                     kubectl_str_pods = str(kubectl_get_pods.stdout.read())
                     kubectl_pods = json.loads(kubectl_str_pods)
                     for pod in kubectl_pods['items']:
+                        new_service = '/skydns/local/{}/{}/{}/{}:{}'.format(
+                            service['metadata']['app_env'] if 'app_env' in service['metadata'] else 'beta',
+                            service['metadata']['namespace'],
+                            match_port['name'],
+                            pod['status']['podIP'],
+                            match_port['port']
+                        )
                         pprint(client.set(
-                            '/skydns/local/{}/{}/{}/{}:{}'.format(
-                                service['metadata']['app_env'] if 'app_env' in service['metadata'] else 'beta',
-                                service['metadata']['namespace'],
-                                match_port['name'],
-                                pod['status']['podIP'],
-                                match_port['port']
-                            ),
+                            new_service,
                             json.dumps({
                                 'host': pod['status']['podIP'],
                                 'port': match_port['port']
                             })
                         ))
+                        yield new_service
                 except KeyError:
                     pprint('Error: {}'.format(match_port['name']))
+        
+        try:
+            etcd_nodes = client.read('/skydns/local/{}'.format(os.getenv('CLUSTER', 'beta')), recursive=True)
+            for key in set((app.key for app in etcd_nodes.leaves)).difference(set_etcd_services()):
+                if ':' in app.key.split('/')[-1]:
+                    client.delete(key)
+        except etcd.EtcdKeyNotFound:
+            pprint('NO key')
 
         time.sleep(os.getenv('ETCD_SERVICE_INTERVAL', 10))
 
